8bb77331b81c84aaadd02f0fa49a2f94c8ea5aa8
Boseong <lbs6603@gmail.com>
2023-01-04 11:48:56 +0900

    Method

---
+++ 06_Method/Calculator.cs
@@ -0,0 +1,31 @@
+﻿namespace Method
+{
+    class Calculator
+    {
+        public static int Plus(int a, int b)
+        {
+            Console.WriteLine("Input : {0}, {1}", a, b);
+
+            int result = a + b;
+            return result;
+        }
+
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+    class MainApp
+    {
+        public static void Main()
+        {
+            int result = Calculator.Plus(3, 4);
+            Console.WriteLine(result);
+
+            result = Calculator.Minus(5, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
+
